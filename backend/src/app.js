31ec171bb79d0bf47dd4df5115b08a5825e36ed5
Meet Parmar <parmarmeet425@gmail.com>
2026-06-06 18:22:55 +0530

    Fix CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,13 +5,7 @@ import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
